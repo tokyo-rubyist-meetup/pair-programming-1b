5d433c7397abe2d7905ad168a9d7483a025165cc
mayuko hirono <mayuko.hirono@gmail.com>
2012-10-21 16:47:29 +0900

    Showa date support
    Natsukashii na~

--- calendar.rb
+++ calendar.rb
@@ -3,7 +3,11 @@ module Calendar
   def self.convert(date)
     return nil unless date
     year, month, day = date.split('-')
-    japanese_year = year.to_i - 1989 + 1
-    "平成#{japanese_year}年#{month}月#{day}日"
+    if year.to_i >= 1989
+      japanese_year = "平成#{year.to_i - 1989 + 1}"
+    else
+      japanese_year = "昭和#{year.to_i - 1926 + 1}"
+    end
+    "#{japanese_year}年#{month}月#{day}日"
   end
 end
--- calendar_spec.rb
+++ calendar_spec.rb
@@ -12,5 +12,9 @@ describe Calendar do
   it "should work for today" do
     Calendar.convert("2012-10-20").should == "平成24年10月20日"
   end
+
+it "should work for Showa dates (natsukashii)" do
+  Calendar.convert("1979-08-08").should == "昭和54年08月08日"
+end
 end
 
