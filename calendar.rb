8b66432122d2f30c30ec89f135d04e78e8933fca
mayuko hirono <mayuko.hirono@gmail.com>
2012-10-21 17:06:49 +0900

    Support for start of Heisei on 1989-01-08

--- calendar.rb
+++ calendar.rb
@@ -2,12 +2,12 @@
 module Calendar
   def self.convert(date)
     return nil unless date
-    year, month, day = date.split('-')
-    if year.to_i >= 1989
-      japanese_year = "平成#{year.to_i - 1989 + 1}"
+    year, month, day = date.split('-').map(&:to_i)
+    if Date.parse(date) >= Date.parse("1989-01-08")
+      japanese_year = "平成#{(year - 1989 + 1).to_s.rjust(2, '0')}"
     else
-      japanese_year = "昭和#{year.to_i - 1926 + 1}"
+      japanese_year = "昭和#{(year - 1926 + 1).to_s.rjust(2, '0')}"
     end
-    "#{japanese_year}年#{month}月#{day}日"
+    "#{japanese_year}年#{month.to_s.rjust(2, '0')}月#{day.to_s.rjust(2, '0')}日"
   end
 end
--- calendar_spec.rb
+++ calendar_spec.rb
@@ -13,8 +13,14 @@ describe Calendar do
     Calendar.convert("2012-10-20").should == "平成24年10月20日"
   end
 
-it "should work for Showa dates (natsukashii)" do
-  Calendar.convert("1979-08-08").should == "昭和54年08月08日"
-end
+  it "should work for border of era" do
+    Calendar.convert("1989-01-07").should == "昭和64年01月07日"
+    Calendar.convert("1989-01-08").should == "平成01年01月08日"
+    Calendar.convert("1989-01-09").should == "平成01年01月09日"
+  end
+
+  it "should work for Showa dates (natsukashii)" do
+    Calendar.convert("1979-08-08").should == "昭和54年08月08日"
+  end
 end
 
