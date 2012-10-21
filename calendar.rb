120efcdbe742c48e812163ba0cf1c04752ca062d
mayuko hirono <mayuko.hirono@gmail.com>
2012-10-21 16:39:43 +0900

    Heisei support
    Heisei jump!

--- calendar.rb
+++ calendar.rb
@@ -1,4 +1,9 @@
+#encoding: utf-8
 module Calendar
   def self.convert(date)
+    return nil unless date
+    year, month, day = date.split('-')
+    japanese_year = year.to_i - 1989 + 1
+    "平成#{japanese_year}年#{month}月#{day}日"
   end
 end
--- calendar_spec.rb
+++ calendar_spec.rb
@@ -8,5 +8,9 @@ describe Calendar do
   it "should return a blank string for nil input" do
     Calendar.convert(nil).should == nil
   end
+  
+  it "should work for today" do
+    Calendar.convert("2012-10-20").should == "平成24年10月20日"
+  end
 end
 
